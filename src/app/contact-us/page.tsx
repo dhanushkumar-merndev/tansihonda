91ec1ec141136897f0d32448f98210306494b630
Dhanush Kumar <dhanushkumar.merndev@gmail.com>
2026-02-10 12:37:43 +0530

    Final deployment with analytics and contact change

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -8,7 +8,7 @@ export default function Scooter() {
 
         {/* TOP WHITE AREA */}
         <div className="absolute top-0 left-0 w-full h-[8vh] md:h-[10vh] bg-white overflow-hidden z-10">
-          <Link href="/" className="absolute top-2 right-4 w-[120px] md:w-[180px] h-full">
+          <Link href="/" className="absolute top-2 -right-4 w-[120px] md:w-[180px] h-full">
             <Image
               src="/logo.svg"
               alt="Logo"
